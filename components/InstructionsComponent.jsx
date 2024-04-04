9dffb0577c4fa9082657d70962b5d9920f67687c
Daroas <166027977+Daroas@users.noreply.github.com>
2024-04-04 17:08:52 -0300

    Update InstructionsComponent.jsx

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -1,80 +1,104 @@
-import styles from "../styles/InstructionsComponent.module.css";
-import Router, { useRouter } from "next/router";
-export default function InstructionsComponent() {
-	const router = useRouter();
-	return (
-		<div className={styles.container}>
-			<header className={styles.header_container}>
-				<h1>
-					create<span>-web3-dapp</span>
-				</h1>
-				<p>
-					Get started by editing this page in{" "}
-					<span>/pages/index.js</span>
-				</p>
-			</header>
-
-			<div className={styles.buttons_container}>
-				<a
-					target={"_blank"}
-					href={"https://createweb3dapp.alchemy.com/#components"}
-				>
-					<div className={styles.button}>
-						{/* <img src="https://static.alchemyapi.io/images/cw3d/Icon%20Medium/lightning-square-contained-m.svg" width={"20px"} height={"20px"} /> */}
-						<p>Add Components</p>
-					</div>
-				</a>
-				<a
-					target={"_blank"}
-					href={"https://createweb3dapp.alchemy.com/#templates"}
-				>
-					<div className={styles.button}>
-						{/* <img src="https://static.alchemyapi.io/images/cw3d/Icon%20Medium/lightning-square-contained-m.svg" width={"20px"} height={"20px"} /> */}
-						<p>Explore Templates</p>
-					</div>
-				</a>
-				<a
-					target={"_blank"}
-					href={"https://docs.alchemy.com/docs/create-web3-dapp"}
-				>
-					<div className={styles.button}>
-						<img
-							src="https://static.alchemyapi.io/images/cw3d/Icon%20Large/file-eye-01-l.svg"
-							width={"20px"}
-							height={"20px"}
-						/>
-						<p>Visit Docs</p>
-					</div>
-				</a>
-			</div>
-			<div className={styles.footer}>
-				<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
-					<img
-						id="badge-button"
-						style={{ width: "240px", height: "53px" }}
-						src="https://static.alchemyapi.io/images/marketing/badgeLight.png"
-						alt="Alchemy Supercharged"
-					/>
-				</a>
-				<div className={styles.icons_container}>
-					<div>
-						<a
-							href="https://github.com/alchemyplatform/create-web3-dapp"
-							target={"_blank"}
-						>
-							Leave a star on Github
-						</a>
-					</div>
-					<div>
-						<a
-							href="https://twitter.com/AlchemyPlatform"
-							target={"_blank"}
-						>
-							Follow us on Twitter
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
-}
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Portfólio</title>
+    <style>
+        body {
+            background-color: #f0f0f0;
+            font-family: Arial, Helvetica, sans-serif
+            margin: 0;
+            padding: 0;
+        }
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 30px 0;
+        }
+        #sobre {
+            padding: 20px;
+        }
+        #sobre img {
+            max-width: 100%;
+            height: auto;
+            display: block;
+            margin: 0 auto;
+        }
+        table {
+            width: 80%;
+            margin: 20px auto;
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid #333;
+            padding: 10px;
+        }
+        th {
+            background-color: #333;
+            color: #fff;
+        }
+        form {
+            width: 80%;
+            margin: 20px auto;
+        }
+        form input, form textarea {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 10px;
+            border-radius: 5px;
+            border: 1px solid #333;
+        }
+        form input[type="submit"] {
+            background-color: #333;
+            color: #fff;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+    
+    <header>
+        <h1>Meu Portfólio</h1>
+    </header>
+    <section id="sobre">
+        <h2>Sobre Mim</h2>
+        
+        </style>
+        <p>Meu nome é David Almeida, atuo como técnico em eletrônica na minha empresa LIG CEL.
+            Sou graduado em Ciências Contábeis.
+            Estou cursando atualmente o curso de Desenvolvimento de Sistemas pelo IF Sudeste.
+            Estou interessado em aprimorar meus conhecimentos na área de Tecnologia da Informação.</p>
+        <img src="img/dvd.png" alt="Minha Foto">
+    </section>
+    <section id="habilidades">
+        <h2>Habilidades</h2>
+        <table>
+            <tr>
+                <th>Habilidade</th>
+                <th>Nível</th>
+            </tr>
+            <tr>
+                <td>Eletro Eletronica</td>
+                <td>Possuo curso técnico basico ministrado pela UFV</td>
+            </tr>
+            <tr>
+                <td>Informática</td>
+                <td>Conhecimento amplo em Hardware e Software</td>
+            </tr>
+            <tr>
+                <td>Fotografia</td>
+                <td>Tenho curso da área e câmeras profissionais que uso como um hobby</td>
+            </tr>
+        </table>
+    </section>
+    <section id="contato">
+        <h2>Entre em Contato comigo diretamente</h2>
+	<a href="https://whats.link/falecomdavidalmeida"  target="_blank"><img src="img/whatsapp.png" height="50" width="50" title="Whatsapp"></a>
+	<a href="https://www.instagram.com/dvdalmeidaa"  target="_blank"><img src="img/Instagram.png" height="50" width="50" title="Instagram"></a>
+	<a href="https://www.facebook.com/davidligcel"  target="_blank"><img src="img/facebook.png" height="50" width="50" title="Facebook"></a>
+	<a href="https://www.linkedin.com/in/davidalmeida1905/"  target="_blank"><img src="img/linkedin.png" height="50" width="50" title="Linkedin"></a>
+    <a href="https://t.me/Daroas"  target="_blank"><img src="img/telegram.png" height="50" width="50" title="telegram"></a>
+    </section>
+</body>
+</html>
